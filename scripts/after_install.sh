1d9b15b51ae7a40116b0f5694bb1f0ecadf60816
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-03-01 23:50:30 -0500

    Docker image-17

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
-# kill any servers that may be running in the background 
+# Kill any servers that may be running in the background
 sudo pkill -f runserver
 
-# kill frontend servers if you are deploying any frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
-
+# Change directory to the Django project root
 cd /home/ubuntu/Django-Training/
 
-# # activate virtual environment
-# python3 -m venv venv
-# source venv/bin/activate
-
-# install requirements.txt
-# pip install -r /home/ubuntu/Django-Training/requirements.txt
-# # run server
-# screen -d -m python3 manage.py runserver 0:8000
+# Install the AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
+# Verify AWS CLI installation and configure if needed
 aws --version
-aws configure
+# aws configure  # Uncomment this line if you need to configure AWS CLI
 
+# Set AWS credentials and region (consider using secure methods)
 export AWS_ACCESS_KEY_ID=AKIAYS2NV6GEEGQGLJGM
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=ap-south-1
 
+# Log in to Amazon ECR registry
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 590184051080.dkr.ecr.ap-south-1.amazonaws.com
-# sudo docker pull 590184051080.dkr.ecr.ap-south-1.amazonaws.com/django:latest
 
+# Pull the Docker image from Amazon ECR (if needed)
+sudo docker pull 590184051080.dkr.ecr.ap-south-1.amazonaws.com/django:latest
 
-sudo docker-compose build
-sudo docker-compose up
+# Build and start the Docker Compose services
+sudo docker-compose up -d
