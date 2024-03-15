e0822686e855ce0456c34476ffb90a6dc09078ea
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-03-15 07:46:10 -0400

    Before-Install Chnages

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -15,7 +15,7 @@ sudo ./install auto
 sudo yum update
 # sudo yum install -y python3 python3-dev python3-pip python3-venv
 # pip install --user --upgrade virtualenv
-sudo yum install docker.io -y
+sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
