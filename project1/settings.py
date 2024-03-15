755d31bc01fe2b89d5d6aed95fcef8efcf0ee5e9
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-03-15 12:38:30 -0400

    Added Amplify Url

--- project1/settings.py
+++ project1/settings.py
@@ -75,7 +75,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://3.108.44.64:3000',
     'http://13.233.83.63:3000',
-    'https://master.d1mzkxxxp9hvbh.amplifyapp.com',
+    'https://master.d26g62xkrx3yrj.amplifyapp.com/',
 ]
 
 ROOT_URLCONF = 'project1.urls'
